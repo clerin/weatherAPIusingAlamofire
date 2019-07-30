6ab2a0265a4df1162b0f8b236a8ae584333044aa
clerin <clerinbinil@gmail.com>
2019-07-30 13:30:23 -0400

    included pod files

---
+++ Podfile
@@ -0,0 +1,11 @@
+
+ platform :ios, '9.0'
+
+target 'apiUsingAlamofire' do
+  use_frameworks!
+
+ pod 'SwiftyJSON'
+pod 'Alamofire'
+pod 'ProgressHUD'
+
+end
---
+++ Pods/Target Support Files/Pods-apiUsingAlamofire/Pods-apiUsingAlamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_apiUsingAlamofire : NSObject
+@end
+@implementation PodsDummy_Pods_apiUsingAlamofire
+@end
