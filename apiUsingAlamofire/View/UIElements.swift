fc59218dd411aa3226f3ebf11503c6d6ccc8a5ae
clerin <clerinbinil@gmail.com>
2019-07-31 14:06:35 -0400

    new class for UI added

---
+++ apiUsingAlamofire/View/UIElements.swift
@@ -0,0 +1,26 @@
+//
+//  UIElements.swift
+//  apiUsingAlamofire
+//
+//  Created by clerin binil on 31/07/19.
+//  Copyright © 2019 clerin binil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func textStyle(){
+        self.backgroundColor = UIColor.white
+    }
+}
+
+extension UILabel{
+    
+    func labelStyle(){
+         self.backgroundColor = UIColor.gray
+         self.textColor = UIColor.white
+    }
+}
+
+
